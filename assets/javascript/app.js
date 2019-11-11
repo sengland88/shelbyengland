17c6072f988dc10d6db394eadf77c38430de4a14
Shelby England <Shelby.England@knights.ucf.edu>
2019-11-11 13:49:49 -0500

    combined html into one and working on dynamically creating modals for my projects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,10 +25,73 @@ AOS.init({
 
 });
 
+let portfolio = [
+    {
+        name: "PokeMasters PokeDex",
+        image: "assets/images/pika.jpg",
+        description: "The PokeDex is a Pokemon Trainer's best resource and most important tool. It's time to go exploring.",
+        github: "https://github.com/sengland88/pokedex",
+        url: "https://sengland88.github.io/pokedex/"
+    }, 
+    {
+        name: "Assemble the Avengers",
+        image: "assets/images/the avengers.jpg",
+        description: "Help the Avengers remember who they are by loading GIFs into a special webpage built to help recover their memories.", 
+        github: "https://github.com/sengland88/GIPHY",
+        url: "https://sengland88.github.io/GIPHY/"
+    },
+    {
+        name: "Save the Pokemon!",
+        image: "assets/images/the pokemon.png",
+        description: "Team Rocket is at it again! This time, our heroes cant do it alone. They need your help!",
+        github: "https://github.com/sengland88/unit-4-game",
+        url: "https://sengland88.github.io/unit-4-game/"
+    },
+    {
+        name: "The Pro-Bending Championship",
+        image: "assets/images/the avatar.jpg",
+        description: "Fight your way to the top and become the 2019 Pro-Bending Champion!",
+        github: "https://github.com/sengland88/unit-4-game_RPG",
+        url: "https://sengland88.github.io/unit-4-game_RPG/"
+    },
+    {
+        name: "Grey's Anatomy — A Trivia Game!",
+        image: "assets/images/the greys.jpg",
+        description: "Think you know Grey's Anatomy? Test your knowledge by taking this quiz!",
+        github: "https://github.com/sengland88/TriviaGame", 
+        url: "https://sengland88.github.io/TriviaGame/"
+    },
+    {
+        name: "Train Time",
+        image: "assets/images/train tracks.jpg",
+        description: "It's first day of training as the next Train Station Manager. Get to know your Train Scheduler.",
+        github: "https://github.com/sengland88/Train-Time",
+        url: "https://sengland88.github.io/Train-Time/"
+    },
+    {
+        name: "TV Show Hangman Game",
+        image: "assets/images/the hangman.jpg",
+        description: "We all have our favorite TV shows. Go ahead and try to guess the TV show before time runs out.",
+        github: "https://github.com/sengland88/Word-Guess-Game",
+        url: "https://sengland88.github.io/unit-4-game/"
+    },
+    {
+        name: "The Psychic Game",
+        image: "assets/images/the psychic.jpg",
+        description: "Consider yourself lucky or intuitive? Let's see find out. Try to guess the random letter of the alphabet.",
+        github: "https://github.com/sengland88/Psychic-Game",
+        url: "https://sengland88.github.io/Psychic-Game/"
+    }
+]
+
+for (let i = 0 ; i < portfolio.length ; i++ ) {
+    console.log(portfolio[i])
+}
+
 // index on click functions
 
-$(".strengths").on("click", function() {
-    $(".strengthsFinder").modal('show')
+$(".strengths").on("click", function() {    
+    $(".my-modal").modal('show')
 })
 
 $(".stand").on("click", function() {
@@ -39,44 +102,73 @@ $(".sight").on("click", function() {
     $(".fourSight").modal('show')
 })
 
+
 // portfolio on click functions
 
-$(".pokeDex").on("click", function() {
-    console.log("this works")
-    $(".thePokeDex").modal('show')
+$(".theProject").on("click", function() {
+    
+    let project = $(this).attr("value")
+    console.log(project)
+    createModal(project)
 })
 
 $(".avengers").on("click", function() {
-    console.log("this works")
-    $(".theAvengers").modal('show')
+
 })
 
 $(".pokemon").on("click", function() {
-    console.log("this works")
-    $(".thePokemon").modal('show')
+
 })
 
 $(".korra").on("click", function() {
-    console.log("this works")
-    $(".theKorra").modal('show')
+
 })
 
 $(".greys").on("click", function() {
-    console.log("this works")
-    $(".theGreys").modal('show')
+
 })
 
 $(".train").on("click", function() {
-    console.log("this works")
-    $(".theTrain").modal('show')
+
 })
 
 $(".hangman").on("click", function() {
-    console.log("this works")
-    $(".theHangman").modal('show')
+
 })
 
 $(".psychic").on("click", function() {
-    console.log("this works")
-    $(".thePsychic").modal('show')
+
 })
+
+
+function createModal(project) {
+
+     for (let i = 0; i < portfolio.length; i++) {
+        let modal = $("<div>").addClass("modal fade")
+            .attr("id" , `${project}`)
+            .attr("tabindex", "-1")
+            .attr("role", "dialog")
+            .attr("aria-labelledby", "exampleModalCenterTitle")
+            .attr("aria-hidden", "true")
+        let modalDialog = $("<div>").addClass("modal-dialog modal-dialog-centered")
+            .attr("role", "document")
+        let modalContent = $("<div>").addClass("modal-content")
+        let modalHeader = $("<div>").addClass("modal-header")
+            .html(`<h5 class="modal-title" id="exampleModalLongTitle"></h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>`)
+        let modalBody = $("<div>").addClass("modal-body")
+        let modalFooter = $("<div>").addClass("modal-footer")
+            .html(`<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary"`)
+        modalContent.append(modalHeader)
+        modalContent.append(modalBody)
+        modalContent.append(modalFooter)
+        modalDialog.append(modalContent)
+        modal.append(modalDialog)
+        $(".myProjects").append(modal)
+        $("body").modal('show')
+        console.log("this is the end")
+    }
+}
